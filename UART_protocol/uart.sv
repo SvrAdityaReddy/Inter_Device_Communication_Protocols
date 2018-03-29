2d05eb09094d809754d0464d50be5f2699c9fa7b
SvrAdityaReddy <Aditya.seelapureddy@yahoo.com>
2018-03-29 23:34:57 +0530

    added testbench verilog code

--- UART_protocol/uart.sv
+++ UART_protocol/uart.sv
@@ -6,23 +6,22 @@ module uart (clk, reset, rw, databus, Rx, Tx);
     input Rx;
     output reg Tx;
     // internal variables
-    // reg [7:0] Tx_reg;
     reg [2:0] Tx_count;
     reg [7:0] Rx_reg;
     reg [2:0] Rx_count;
-    always_ff @(posedge clk) begin
+    always @(posedge clk) begin
         if(rw==0) begin
             if(reset) begin
-                // Tx_reg='b000000;
-                Tx= 1;   
-                Tx_count=0;    
+                Tx<=1;   
+                Tx_count<=0;    
             end
             else begin
                 if(Tx_count==0) begin
                     Tx<=0;
+                    Tx_count<=Tx_count+1;
                 end
                 if(Tx_count > 0 && Tx_count < 9) begin
-                    // Tx<=databus[Tx_count];
+                    Tx<=databus[Tx_count];
                     Tx_count<=Tx_count+1;
                 end
                 if(Tx_count==9) begin
@@ -33,17 +32,22 @@ module uart (clk, reset, rw, databus, Rx, Tx);
         end 
         else begin
             if(reset) begin
-                Rx_reg<='b000000;    
+                Rx_reg<=8'b00000000;
+                Rx_count<=0;
+                Tx<=1;    
             end
             else begin
-                if(Tx_count==0) begin
+                if(Rx_count==0) begin
+                    Tx<=Rx;
                     Rx_count<=Rx_count+1;
                 end
                 if(Rx_count > 0 && Rx_count < 9) begin
                     Rx_reg[Rx_count-1]<=Rx;
                     Rx_count<=Rx_count+1;
+                    Tx<=Rx;
                 end
-                if(Tx_count==9) begin
+                if(Rx_count==9) begin
+                    Tx<=1;
                     Rx_count<=Rx_count+1;
                 end
             end
--- UART_protocol/uart_test.v
+++ UART_protocol/uart_test.v
@@ -1,3 +1,67 @@
 module uartTest;
-    
+    // for instance 1 
+    reg clk, reset, rw, Rx;
+    reg [7:0] databus;
+    wire Tx;
+    // for instance 2
+    reg clk2, reset2, rw2, Rx2;
+    reg [7:0] databus2;
+    wire Tx2;
+    // memory_design G(data_output, data_input, address, enable, read_write, clk);
+    uart uart_tx(clk, reset, rw, databus, Rx, Tx);
+    uart uart_rx(clk2, reset2, rw2, databus2, Tx, Tx2);
+    initial begin
+        reset=1;
+        reset2=1;
+        Rx=0;
+        #10 rw=0;
+        #10 rw2=1;
+        #20 databus=8'b01010101;
+        #35 reset=0;
+        #35 reset2=0; 
+    end
+    always begin
+        #30 clk=1;
+        #35 clk=0;
+        #40 clk=1;
+        #45 clk=0;
+        #50 clk=1;
+        #55 clk=0;
+        #60 clk=1;
+        #65 clk=0;
+        #70 clk=1;
+        #75 clk=0;
+        #80 clk=1;
+        #85 clk=0;
+        #90 clk=1;
+        #95 clk=0;
+        #100 clk=1;
+        #105 clk=0;
+        #110 clk=1;
+        #115 clk=0;
+        #120 clk=1;
+        #125 clk=0;
+    end
+    always begin
+        #30 clk2=1;
+        #35 clk2=0;
+        #40 clk2=1;
+        #45 clk2=0;
+        #50 clk2=1;
+        #55 clk2=0;
+        #60 clk2=1;
+        #65 clk2=0;
+        #70 clk2=1;
+        #75 clk2=0;
+        #80 clk2=1;
+        #85 clk2=0;
+        #90 clk2=1;
+        #95 clk2=0;
+        #100 clk2=1;
+        #105 clk2=0;
+        #110 clk2=1;
+        #115 clk2=0;
+        #120 clk2=1;
+        #125 clk2=0;
+    end
 endmodule
