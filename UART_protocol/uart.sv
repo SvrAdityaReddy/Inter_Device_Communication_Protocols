bc82fa7fbb3d738494ebd2287553a9eec736eb1b
SvrAdityaReddy <Aditya.seelapureddy@yahoo.com>
2018-03-30 11:02:07 +0530

    code under rigorous testing

--- UART_protocol/uart.sv
+++ UART_protocol/uart.sv
@@ -1,15 +1,16 @@
-module uart (clk, reset, rw, databus, Rx, Tx);
+module uart (clk, reset, rw, databus_read, databus_write, Rx, Tx);
     input clk;
     input reset;
     input rw;
-    input [7:0] databus;
+    input reg [7:0] databus_read;
+    output reg [7:0] databus_write;
     input Rx;
     output reg Tx;
     // internal variables
     reg [2:0] Tx_count;
     reg [7:0] Rx_reg;
     reg [2:0] Rx_count;
-    always @(posedge clk) begin
+    always_ff @(posedge clk) begin
         if(rw==0) begin
             if(reset) begin
                 Tx<=1;   
@@ -21,12 +22,12 @@ module uart (clk, reset, rw, databus, Rx, Tx);
                     Tx_count<=Tx_count+1;
                 end
                 if(Tx_count > 0 && Tx_count < 9) begin
-                    Tx<=databus[Tx_count];
+                    Tx<=databus_read[Tx_count];
                     Tx_count<=Tx_count+1;
+                    $display("Tx %b", databus_read[Tx_count]);
                 end
                 if(Tx_count==9) begin
                     Tx<=1;
-                    Tx_count=0;
                 end
             end
         end 
@@ -34,7 +35,8 @@ module uart (clk, reset, rw, databus, Rx, Tx);
             if(reset) begin
                 Rx_reg<=8'b00000000;
                 Rx_count<=0;
-                Tx<=1;    
+                databus_write<=8'b00000000;
+                Tx<=1;                    
             end
             else begin
                 if(Rx_count==0) begin
@@ -44,11 +46,14 @@ module uart (clk, reset, rw, databus, Rx, Tx);
                 if(Rx_count > 0 && Rx_count < 9) begin
                     Rx_reg[Rx_count-1]<=Rx;
                     Rx_count<=Rx_count+1;
+                    $display("Rx_reg %b",Rx_reg);
                     Tx<=Rx;
                 end
                 if(Rx_count==9) begin
                     Tx<=1;
                     Rx_count<=Rx_count+1;
+                    databus_write<=Rx_reg;
+                    $display("Rx_reg final %b",Rx_reg);
                 end
             end
         end
