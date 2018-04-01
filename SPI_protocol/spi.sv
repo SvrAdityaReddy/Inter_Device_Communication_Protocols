1c4d58d7997c28f2e3d86ed59820d399ecc58508
SvrAdityaReddy <Aditya.seelapureddy@yahoo.com>
2018-04-01 19:24:34 +0530

    code updated

--- SPI_protocol/spi.sv
+++ SPI_protocol/spi.sv
@@ -1,7 +1,37 @@
-module spi_master ();
-    
+// SPI mode 1
+
+// 8 bit data tranmitted/received
+
+module spi_master (sclk,MISO,MOSI,ss);
+    input sclk,MISO;
+    output reg ss,MOSI;
+    always_ff @(posedge sclk) begin
+        
+    end
 endmodule
 
-module spi_slave ();
-    
+// 8 bit data tranmitted/received
+
+module spi_slave (sclk,ss,MOSI,MISO);
+    input sclk,MOSI,ss;
+    output reg MISO;
+    reg [7:0] mem;
+    reg [3:0] count;
+    always_ff @(negedge sclk) begin
+        if (ss) begin
+            count<=4'b0000;
+        end
+        if(ss==0 && count>0) begin
+            if(count<8) begin
+                mem[count]<=MOSI;
+                count<=count+1;
+            end
+            else begin
+                if(count>0) begin
+                    MISO<=mem[8-count];
+                    count<=count-1; 
+                end
+            end
+        end
+    end
 endmodule
---
+++ SPI_protocol/spi_test.v
@@ -0,0 +1,3 @@
+module spi_test;
+
+endmodule
