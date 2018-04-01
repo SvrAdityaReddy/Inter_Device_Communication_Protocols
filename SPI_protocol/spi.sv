52ecfebc9f8c644ea21e2081663cb4fd67cb1a41
SvrAdityaReddy <Aditya.seelapureddy@yahoo.com>
2018-04-01 23:07:01 +0530

    spi code added and testing is remaining

--- SPI_protocol/spi.sv
+++ SPI_protocol/spi.sv
@@ -2,19 +2,48 @@
 
 // 8 bit data tranmitted/received
 
-module spi_master (sclk,MISO,MOSI,ss);
-    input sclk,MISO;
-    output reg ss,MOSI;
+module spi_master (sclk,ss,mosi,miso,data_in,data_out);
+    input miso;
+    input reg [7:0] data_in;
+    output reg sclk,ss,mosi;
+    output reg [7:0] data_out;
+    // internal variable
+    reg [3:0] count;
+    initial begin
+        sclk=0;
+        forever begin
+            #100 sclk=~sclk;
+        end
+    end
     always_ff @(posedge sclk) begin
-        
+        if(count==0) begin
+            ss<=0;
+            mosi<=data_in[count];
+            count<=count+1;
+        end
+        if(count>0) begin 
+            if(count<8) begin
+                mosi=data_in[count];
+                count<=count+1;
+            end
+            else begin
+                if(count>0) begin
+                    data_out[8-count]=miso;
+                    count<=count-1;
+                end
+                else begin
+                    count<=0;
+                end
+            end
+        end    
     end
 endmodule
 
 // 8 bit data tranmitted/received
 
-module spi_slave (sclk,ss,MOSI,MISO);
-    input sclk,MOSI,ss;
-    output reg MISO;
+module spi_slave (sclk,ss,mosi,miso);
+    input sclk,mosi,ss;
+    output reg miso;
     reg [7:0] mem;
     reg [3:0] count;
     always_ff @(negedge sclk) begin
@@ -23,12 +52,12 @@ module spi_slave (sclk,ss,MOSI,MISO);
         end
         if(ss==0 && count>0) begin
             if(count<8) begin
-                mem[count]<=MOSI;
+                mem[count]<=mosi;
                 count<=count+1;
             end
             else begin
                 if(count>0) begin
-                    MISO<=mem[8-count];
+                    miso<=mem[8-count];
                     count<=count-1; 
                 end
             end
--- SPI_protocol/spi_test.v
+++ SPI_protocol/spi_test.v
@@ -1,3 +1,14 @@
 module spi_test;
+    wire miso;
+    reg [7:0] data_in;
+    wire sclk,ss,mosi;
+    wire [7:0] data_out;
+    spi_master spi_m(sclk,ss,mosi,miso,data_in,data_out);
+    spi_slave spi_s(sclk,ss,mosi,miso);
+    initial begin
+        data_in=8'b11101010;
+    end
+    always begin
 
+    end
 endmodule
