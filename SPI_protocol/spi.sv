a7a09b26dfdd1d4f07f4667e58982ff8900e2689
SvrAdityaReddy <Aditya.seelapureddy@yahoo.com>
2018-04-02 21:34:14 +0530

    code updated

--- SPI_protocol/spi.sv
+++ SPI_protocol/spi.sv
@@ -2,63 +2,83 @@
 
 // 8 bit data tranmitted/received
 
-module spi_master (sclk,ss,mosi,miso,data_in,data_out);
-    input miso;
+module spi_master (sclk,ss,rst,mosi,miso,data_in,data_out);
+    input miso,rst;
     input reg [7:0] data_in;
     output reg sclk,ss,mosi;
     output reg [7:0] data_out;
     // internal variable
-    reg [3:0] count;
+    reg [5:0] count;
+    reg [7:0] mem;
     initial begin
         sclk=0;
+        ss=1;
+        #10 ss=0;
         forever begin
             #100 sclk=~sclk;
         end
     end
     always_ff @(posedge sclk) begin
-        if(count==0) begin
-            ss<=0;
-            mosi<=data_in[count];
-            count<=count+1;
+        if(rst) begin
+            count<=0;
         end
-        if(count>0) begin 
-            if(count<8) begin
-                mosi=data_in[count];
-                count<=count+1;
-            end
-            else begin
-                if(count>0) begin
-                    data_out[8-count]=miso;
-                    count<=count-1;
+        if(rst==0) begin
+            if(count>=0) begin 
+                if(count<8) begin
+                    mosi<=data_in[count];
+                    count<=count+1;
                 end
                 else begin
-                    count<=0;
+                    if(count<16) begin
+                        mem[count-8]<=miso;
+                        count<=count+1;
+                    end
+                    else begin
+                        data_out<=mem;
+                        count<=0;
+                    end
                 end
-            end
-        end    
+            end    
+        end
     end
 endmodule
 
 // 8 bit data tranmitted/received
 
-module spi_slave (sclk,ss,mosi,miso);
-    input sclk,mosi,ss;
+module spi_slave (sclk,ss,rst,mosi,miso);
+    input sclk,mosi,ss,rst;
     output reg miso;
     reg [7:0] mem;
-    reg [3:0] count;
+    reg [5:0] count;
     always_ff @(negedge sclk) begin
+        $display("count sclk = %b", count);
         if (ss) begin
-            count<=4'b0000;
+            count=0;
+            $display("count sclk initial = %b", count);
         end
-        if(ss==0 && count>0) begin
-            if(count<8) begin
-                mem[count]<=mosi;
-                count<=count+1;
-            end
-            else begin
-                if(count>0) begin
-                    miso<=mem[8-count];
-                    count<=count-1; 
+        if(ss==0 && rst==0) begin
+            if(count>=0) begin
+                if(count<7) begin
+                    mem[count]=mosi;
+                    count=count+1;
+                end
+                else begin 
+                    if(count==7) begin
+                        mem[count]=mosi;
+                        count=count+1;
+                        miso=mem[count-8];
+                        count=count+1; 
+                    end
+                    else begin
+                        if(count<16) begin
+                            miso<=mem[count-8];
+                            count<=count+1; 
+                            $display("count outside = %b ,,,, %b,,,,,%b", count,miso,mem);
+                        end
+                        else begin
+                            count<=0;
+                        end
+                    end
                 end
             end
         end
--- SPI_protocol/spi_test.v
+++ SPI_protocol/spi_test.v
@@ -1,14 +1,22 @@
+module data_path(data_in,sclk,rst,data_out,ss,mosi,miso);
+    input [7:0] data_in;
+    input rst;
+    output [7:0] data_out;
+    output ss,mosi,miso,sclk;
+    spi_master spi_m(sclk,ss,rst,mosi,miso,data_in,data_out);
+    spi_slave spi_s(sclk,ss,rst,mosi,miso);
+endmodule
+
 module spi_test;
     wire miso;
     reg [7:0] data_in;
+    reg rst;
     wire sclk,ss,mosi;
     wire [7:0] data_out;
-    spi_master spi_m(sclk,ss,mosi,miso,data_in,data_out);
-    spi_slave spi_s(sclk,ss,mosi,miso);
+    data_path dp(data_in,sclk,rst,data_out,ss,mosi,miso);
     initial begin
+        rst=1;
         data_in=8'b11101010;
-    end
-    always begin
-
+        #250 rst=0;
     end
 endmodule
