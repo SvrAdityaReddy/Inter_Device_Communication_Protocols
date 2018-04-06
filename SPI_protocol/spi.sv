d6d85864c0a359568995b6321c2ba5d447222c66
SvrAdityaReddy <Aditya.seelapureddy@yahoo.com>
2018-04-06 08:57:31 +0530

    spi code updated mode1

--- SPI_protocol/spi.sv
+++ SPI_protocol/spi.sv
@@ -1,4 +1,4 @@
-// SPI mode 1
+// SPI MODE 1
 
 // 8 bit data tranmitted/received
 
@@ -46,6 +46,7 @@ module spi_master (sclk,ss,rst,mosi,miso,data_in,data_out);
                     count_recv<=count_recv+1;
                 end
                 else begin
+                    data_out<=mem;
                     count_recv<=0;
                 end
             end    
@@ -55,38 +56,43 @@ endmodule
 
 // 8 bit data tranmitted/received
 
-module spi_slave (sclk,ss,rst,mosi,miso);
+module spi_slave (sclk,ss,rst,mosi,miso,data_transfer);
     input sclk,mosi,ss,rst;
+    input [7:0] data_transfer;
     output reg miso;
     reg [7:0] mem;
     reg [3:0] count_send;
     reg [3:0] count_recv;
     always_ff @(negedge sclk) begin
         if (ss) begin
-            count=0;
+            count_recv<=0;
         end
         if(ss==0 && rst==0) begin
-            if(count>=0) begin
-                if(count<7) begin
-                    mem[count]=mosi;
-                    count=count+1;
+            if(count_recv>=0) begin
+                if(count_recv<8) begin
+                    mem[count_recv]<=mosi;
+                    count_recv<=count_recv+1;
                 end
-                else begin 
-                    if(count==7) begin
-                        mem[count]=mosi;
-                        count=count+1;
-                        miso=mem[count-8];
-                        count=count+1; 
-                    end
-                    else begin
-                        if(count<16) begin
-                            miso<=mem[count-8];
-                            count<=count+1; 
-                        end
-                        else begin
-                            count<=0;
-                        end
-                    end
+                else begin
+                    count_recv<=0;
+                end
+            end
+        end
+    end
+    always_ff @(posedge sclk) begin
+        if (rst) begin
+            count_send<=0;
+        end
+        $display("miso %b",count_send);
+        if(ss==0 && rst==0) begin
+            if(count_send>=0) begin
+                $display("miso %b",data_transfer[count_recv]);
+                if(count_send<8) begin
+                    miso<=data_transfer[count_send];
+                    count_send<=count_send+1;
+                end
+                else begin
+                    count_send<=0;
                 end
             end
         end
--- SPI_protocol/spi_test.v
+++ SPI_protocol/spi_test.v
@@ -1,26 +1,27 @@
 // to connect master and slave
 
-module data_path(data_in,sclk,rst,data_out,ss,mosi,miso);
-    input [7:0] data_in;
+module data_path(data_in,data_transfer,sclk,rst,data_out,ss,mosi,miso);
+    input [7:0] data_in, data_transfer;
     input rst;
     output [7:0] data_out;
     output ss,mosi,miso,sclk;
     spi_master spi_m(sclk,ss,rst,mosi,miso,data_in,data_out);
-    spi_slave spi_s(sclk,ss,rst,mosi,miso);
+    spi_slave spi_s(sclk,ss,rst,mosi,miso,data_transfer);
 endmodule
 
 // testbench
 
 module spi_test;
     wire miso;
-    reg [7:0] data_in;
+    reg [7:0] data_in,data_transfer;
     reg rst;
     wire sclk,ss,mosi;
     wire [7:0] data_out;
-    data_path dp(data_in,sclk,rst,data_out,ss,mosi,miso);
+    data_path dp(data_in,data_transfer,sclk,rst,data_out,ss,mosi,miso);
     initial begin
         rst=1;
         data_in=8'b11101010;
+        data_transfer=8'b11101110;
         #250 rst=0;
     end
 endmodule
